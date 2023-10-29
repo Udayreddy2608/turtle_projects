c8a7cd5659c26f75ddc7730b18466b16c7a66697
Uday Kiran Reddy Saddipally <143345142+Udayreddy2608@users.noreply.github.com>
2023-10-29 10:16:09 +0530

    Update spirograph.py

--- spirograph.py
+++ spirograph.py
@@ -1,7 +1,7 @@
 from turtle import Turtle, Screen
 import turtle
 import random
-niki=Turtle()
+joey=Turtle()
 turtle.colormode(255)
 def random_color():
     r=random.randint(0,255)
@@ -9,12 +9,12 @@ def random_color():
     b=random.randint(0,255)
     colour=(r,g,b)
     return colour
-niki.speed(0)
+joey.speed(0)
 def draw_spiro(size_of_gap):
     for i in range(360//size_of_gap):
-        niki.color(random_color())
-        niki.circle(100)
-        niki.setheading(niki.heading()+size_of_gap)
+        joey.color(random_color())
+        joey.circle(100)
+        joey.setheading(joey.heading()+size_of_gap)
 draw_spiro(5)
 
 
